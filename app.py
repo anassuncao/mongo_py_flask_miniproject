7e8c23ae70f6d5874bfe4f1b4e777bf5bb490bd4
anassuncao <ana.v.assuncao@gmail.com>
2020-07-07 15:39:12 +0000

    commit at the end of 7th july. A lot more lessons to go

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 import os
 # First we import Flask
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 # Secondly we create an instance of the Flask and store it in the variable app
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://mainUser:m4inUs3r@myfirstcluster.ozrtj.mongodb.net/task_manager?retryWrites=true&w=majority'
+
+# Create an instance of pymongo
+mongo = PyMongo(app)
+
+
 # Now we create a test function
 @app.route('/') # The '/' refers to the default route
-def hello():
-    return 'Hello World!... again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
